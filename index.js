30df1f8251f5cf3ecec94a02d4746c8bf5ca8b07
Michael Perez <mperez6473@gmail.com>
2020-12-16 12:43:03 -0600

    Images upload to folder in directory

--- index.js
+++ index.js
@@ -8,6 +8,31 @@ const router = express.Router();
 
 const port = process.env.PORT || 8080;
 
+const upload = multer({
+  dest: "images/",
+  limits: { fileSize: 10000000, files: 1 },
+  fileFilter: (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|JPG)$/)) {
+      return callback(new Error("Only jpg, jpeg, JPG allowed!"), false);
+    }
+    callback(null, true);
+  }
+}).single("image");
+
+router.post("/images/upload", (req, res) => {
+  upload(req, res, function (err) {
+    const curr_img = req.file.filename;
+    if (err) {
+      res.status(400).json({ message: err.message });
+    } else {
+      let path = `/images/${curr_img}`;
+      res
+        .status(200)
+        .json({ message: "Image Uploaded Successfully!", path: path });
+    }
+  });
+});
+
 app.use("/", router);
 
 app.use((err, req, res, next) => {
