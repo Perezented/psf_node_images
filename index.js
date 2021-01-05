480aa69b9536a9d161b2f4f91a9e90aaccb32d9d
Michael Perez <mperez6473@gmail.com>
2021-01-05 09:40:06 -0600

    removal of post links to not have anymore images added in the future

--- index.js
+++ index.js
@@ -34,18 +34,25 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
-
 // uploading using multer
-const upload = multer({
-  dest: "images/",
-  limits: { fileSize: 10000000, files: 1 },
-  fileFilter: (req, file, callback) => {
-    // file type check
-    if (!file.originalname.match(/\.(jpg|jpeg|JPG)$/)) {
-      return callback(new Error("Only jpg, jpeg, JPG allowed!"), false);
-    }
-    callback(null, true);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./compressed_images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
   }
+});
+const comp_storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./compressed_images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({
+  storage: storage
 }).single("image");
 const compressed_upload = multer({
   dest: "compressed_images/",
@@ -58,36 +65,35 @@ const compressed_upload = multer({
     callback(null, true);
   }
 }).single("image");
-
 // Upload images using this endpoint
-router.post("/images/upload", (req, res) => {
-  upload(req, res, function (err) {
-    // stored as filename
-    const curr_img = req.file.filename;
-    if (err) {
-      res.status(400).json({ message: err.message });
-    } else {
-      let path = `/images/${curr_img}`;
-      res
-        .status(200)
-        .json({ message: "Image Uploaded Successfully!", path: path });
-    }
-  });
-});
-router.post("/compressed_images/upload", (req, res) => {
-  compressed_upload(req, res, function (err) {
-    // stored as filename
-    const curr_img = req.file.filename;
-    if (err) {
-      res.status(400).json({ message: err.message });
-    } else {
-      let path = `/compressed_images/${curr_img}`;
-      res
-        .status(200)
-        .json({ message: "Image Uploaded Successfully!", path: path });
-    }
-  });
-});
+// router.post("/images/upload", (req, res) => {
+//   upload(req, res, function (err) {
+//     // stored as filename
+//     const curr_img = req.file.filename;
+//     if (err) {
+//       res.status(400).json({ message: err.message });
+//     } else {
+//       let path = `/images/${curr_img}`;
+//       res
+//         .status(200)
+//         .json({ message: "Image Uploaded Successfully!", path: path });
+//     }
+//   });
+// });
+// router.post("/compressed_images/upload", (req, res) => {
+//   compressed_upload(req, res, function (err) {
+//     // stored as filename
+//     const curr_img = req.file.filename;
+//     if (err) {
+//       res.status(400).json({ message: err.message });
+//     } else {
+//       let path = `/compressed_images/${curr_img}`;
+//       res
+//         .status(200)
+//         .json({ message: "Image Uploaded Successfully!", path: path });
+//     }
+//   });
+// });
 
 // get by image filename in the images folder
 router.get("/images/:imagename", (req, res) => {
